91a23cebc72badf7349417892d867b44c82bdbc8
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-08-04 22:00:26 +0100

    #30: Make a page component for random breed page. Fixes #30

--- src/index/renderer.ts
+++ src/index/renderer.ts
@@ -1,10 +1,10 @@
 // In renderer process (web page).
 import { ipcRenderer } from 'electron';
 import { MessageType } from './constants';
-import BreedContainerElem, { BreedDataRenderer } from '../components/BreedContainerElem';
-import GenericButton from '../components/GenericButton';
+import { BreedDataRenderer } from '../components/BreedContainerElem';
 import SideBar from '../components/SideBar';
 import Content from '../components/Content';
+import RandomBreedPage from '../pages/RandomBreedPage';
 
 const container = document.getElementById('container') as HTMLDivElement;
 const sideBar = SideBar([
@@ -14,27 +14,8 @@ const contentElem = Content();
 container.appendChild(sideBar);
 container.appendChild(contentElem);
 
-let breedContainer = document.createElement('div');
-
 async function getNextBreedData(): Promise<BreedDataRenderer | null> {
 	return await ipcRenderer.invoke(MessageType[MessageType.requestNextBreedData]) as BreedDataRenderer | null;
 }
 
-function showBreedData(data: BreedDataRenderer | null) {
-	if (data === null) return;
-	const newBreedContainer = BreedContainerElem(data);
-	// contentElem.appendChild(breedContainer);
-	contentElem.replaceChild(newBreedContainer, breedContainer);
-	breedContainer = newBreedContainer;
-}
-
-async function onNextBreedPress() {
-	const data = await getNextBreedData();
-	showBreedData(data);
-}
-
-const nextBreedButton = GenericButton('Get next breed', 'blue', onNextBreedPress);
-contentElem.appendChild(nextBreedButton);
-contentElem.appendChild(breedContainer);
-
-onNextBreedPress();
+contentElem.appendChild(RandomBreedPage(getNextBreedData));
---
+++ src/pages/RandomBreedPage.ts
@@ -0,0 +1,27 @@
+import BreedContainerElem, { BreedDataRenderer } from '../components/BreedContainerElem';
+import GenericButton from '../components/GenericButton';
+
+const RandomBreedPage = (getNextBreedData: () => Promise<BreedDataRenderer | null>): HTMLDivElement => {
+	const page = document.createElement('div');
+	let breedContainer = document.createElement('div');
+
+	function showBreedData(data: BreedDataRenderer | null) {
+		if (data === null) return;
+		const newBreedContainer = BreedContainerElem(data);
+		page.replaceChild(newBreedContainer, breedContainer);
+		breedContainer = newBreedContainer;
+	}
+
+	async function onNextBreedPress() {
+		const data = await getNextBreedData();
+		showBreedData(data);
+	}
+
+	const nextBreedButton = GenericButton('Get next breed', 'blue', onNextBreedPress);
+	page.appendChild(nextBreedButton);
+	page.appendChild(breedContainer);
+
+	return page;
+};
+
+export default RandomBreedPage;
