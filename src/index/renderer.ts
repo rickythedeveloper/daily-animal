41078464abdb62bb022ebd3b954e960800697100
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-08-10 22:50:30 +0100

    #32: Use Components in BreedThumbnail

--- src/index/renderer.ts
+++ src/index/renderer.ts
@@ -5,6 +5,7 @@ import { BreedDataRenderer } from '../components/BreedContainerElem';
 import SideBar from '../components/SideBar';
 import Content from '../components/Content';
 import RandomBreedPage from '../pages/RandomBreedPage';
+import BreedThumbnail from '../components/BreedThumbnail';
 
 const container = document.getElementById('container') as HTMLDivElement;
 const sideBar = SideBar([
@@ -14,8 +15,24 @@ const contentElem = Content();
 container.appendChild(sideBar);
 container.appendChild(contentElem);
 
+let page = document.createElement('div');
+contentElem.appendChild(page);
+
+function showNewPage(newPage: HTMLDivElement) {
+	contentElem.replaceChild(newPage, page);
+	page = newPage;
+}
+
 async function getNextBreedData(): Promise<BreedDataRenderer | null> {
 	return await ipcRenderer.invoke(MessageType[MessageType.requestNextBreedData]) as BreedDataRenderer | null;
 }
 
-contentElem.appendChild(RandomBreedPage(getNextBreedData).element);
+// contentElem.appendChild(RandomBreedPage(getNextBreedData).element);
+
+function showStuff() {
+	getNextBreedData().then((breed) => {
+		if (breed !== null) showNewPage(BreedThumbnail(breed.breedData).element);
+	});
+}
+
+showStuff();
