8cbed0a3fc5d982104b8a9f0ad23b48853195ff8
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-08-16 20:56:37 +0100

    #44 Make Navbar float

---
+++ src/components/unique/navigationBarDummy.ts
@@ -0,0 +1,6 @@
+import Component from '../../models/Component';
+
+const navigationBarDummy = new Component('div');
+navigationBarDummy.element.id = 'navigationBarDummy';
+
+export default navigationBarDummy;
